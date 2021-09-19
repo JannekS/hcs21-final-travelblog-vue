1da4d1609fb0100d39dee1e281f3dc270f5097cb
Jannek <>
2021-09-19 23:10:15 +0200

    sekched out blog post detail page

---
+++ src/views/Post.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="detail-wrapper">
+    <div class="go-back">
+      <router-link to="/">Back to overview</router-link>
+    </div>
+    <div class="blog-post">
+      <h1>The Shiny Cruise Ship Magnet</h1>
+      <p>
+        <img src="/icons/map-pin.svg" alt="Location: " /> Santorini | Greece
+      </p>
+      <p>
+        <img src="/icons/calendar.svg" alt="Date: " /> 2019-06-05 to 2019-06-08
+      </p>
+      <p>written by:</p>
+      <div class="author-info">
+        <div class="author-img">
+          <img src="/img/authors/jannek.jpg" alt="author-img" />
+        </div>
+        <p>Jannek</p>
+      </div>
+      <div class="post-img">
+        <img src="/img/locations/santorini.jpg" alt="post image" />
+      </div>
+      <div class="main-text">
+        <p>
+          Don't forget to bring your sunglasses. Because the main town sitting
+          on top of the highest spot of the greek Island Santorini is so white
+          that you almost feel blinded by it ;-) Lorem ipsum dolor sit amet
+          consectetur, adipisicing elit. Quisquam magni, hic ducimus praesentium
+          mollitia nesciunt ea dicta cum ipsa omnis quod optio unde voluptatum
+          sed dolorum iste culpa distinctio dignissimos. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Temporibus tempora dolor suscipit.
+          Dolores consectetur et sequi tempora? Obcaecati laudantium eos ipsa
+          modi iusto dolor! Quibusdam praesentium nulla atque id fugit. Lorem
+          ipsum dolor, sit amet consectetur adipisicing elit. Saepe cum ea at
+          rem vero, quae similique repudiandae nobis perferendis itaque alias
+          voluptate beatae harum ipsum quisquam pariatur maiores laudantium
+          non.Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+          adipisci sapiente asperiores numquam, in sit consequatur reiciendis
+          illo harum, ipsum veritatis molestiae, sequi corporis quo. Itaque
+          temporibus similique nam tempore! Lorem ipsum dolor sit amet
+          consectetur, adipisicing elit. Reprehenderit molestias dolores
+          doloremque modi sed dolorem assumenda facere culpa officia, doloribus
+          quam accusantium quidem eum, veritatis animi aliquam. Magnam, atque?
+          Culpa! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla
+          voluptas ullam, beatae labore facilis at quidem, accusamus consectetur
+          eligendi blanditiis voluptatibus autem vitae unde expedita possimus
+          earum qui quas? Officiis?
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
