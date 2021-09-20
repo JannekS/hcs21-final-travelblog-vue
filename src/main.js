897bb7c01951116917be0419c9c2439bf83dcbea
Jannek <>
2021-09-20 20:59:52 +0200

    added maps to main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueGoogleMaps from "@fawmi/vue-google-maps";
 
-createApp(App).use(router).mount("#app");
+createApp(App)
+  .use(router, VueGoogleMaps, {
+    load: {
+      key: process.env.MAPS_API_KEY,
+    },
+  })
+  .mount("#app");
